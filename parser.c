9639b2456dc8825b0656458bd1ab0be6c7af472d
HermaDC <diazcabezas.m1@gmail.com>
2026-03-05 18:16:56 +0100

    feature: new errors in tokenize for numbers and foward declaration of print_error_at()

--- parser.c
+++ parser.c
@@ -5,10 +5,13 @@
 #include <ctype.h>
 
 #include "utils.h"
+
 #define RED "\x1b[1;91m"
 #define RESET "\x1b[0m"
-#define TOK_DELIMITERS " ,;"
 
+#define IS_TOK_DELIMITER(c) (isspace(c) || c == ',' || c == ';')
+
+//TODO: add the creation on sw and tracks sw use a stack
 typedef enum {
     NUMBER,
     OPEN,
@@ -21,6 +24,8 @@ typedef struct {
     TokenType type;
     size_t column;
 } Token;
+
+
 
 // 10 SW(10)
 //token=10 o token=sw(10)
@@ -46,7 +51,16 @@ static int push_token(Token **tokens,
     return 1;
 }
 
-Token *tokenize(const char *str, size_t *num_tokens) {
+void print_error_at(const char *input, size_t column) {
+    fprintf(stderr, "%s\n", input);
+
+    for (size_t i = 0; i < column; i++)
+        fputc(' ', stderr);
+
+    fprintf(stderr, RED "^\n" RESET);
+}
+
+Token *tokenize(const char *str, size_t *num_tokens, int exit_code) {
     size_t buffer_size = 1;
     *num_tokens = 0;
 
@@ -60,13 +74,18 @@ Token *tokenize(const char *str, size_t *num_tokens) {
     while(isspace(token_str[i])) i++;
     while(token_str[i] != '\0'){
         char c = token_str[i];
+        if(IS_TOK_DELIMITER(c)){ //use ,; as delimiters
+            i++;
+            continue;
+        }
         if(isdigit(c)){
             int value = 0;
+            size_t start = 0;
             while(isdigit(token_str[i])){
                 value = value * 10 + (token_str[i] - '0');
-                i++;
+                i++; 
             }
-            if(!push_token(&tokens, num_tokens, &buffer_size, NUMBER, value, i)) goto error;
+            if(!push_token(&tokens, num_tokens, &buffer_size, NUMBER, value, start)) goto error;
         } else if(c == '('){
             if(!push_token(&tokens, num_tokens, &buffer_size, OPEN, 0, i)) goto error;
             i++;
@@ -80,7 +99,11 @@ Token *tokenize(const char *str, size_t *num_tokens) {
             i += 2;
         }
         else {
-            i++;
+            fprintf(stderr, RED "Syntax Error: " RESET "Unexpected character '%c'\n"
+                "Column: %zu\n", c, i);
+            if(c=='s' || c == 'w') fprintf(stderr, "Perhaps you meant SW\n");
+            print_error_at(str, i);
+            goto error;
         }
     }
 
@@ -106,17 +129,21 @@ void print_tokens(Token *tokens_arr, size_t count){
         }
     }
 }
-void print_error_at(const char *input, size_t column) {
-    fprintf(stderr, "%s\n", input);
-
-    for (size_t i = 0; i < column; i++)
-        fputc(' ', stderr);
-
-    fprintf(stderr, RED "^\n" RESET);
-}
 
 int check_syntax(Token *tokens_arr, char *original_str, size_t count){
     int balance = 0;
+    if(tokens_arr[0].type != NUMBER){
+        fprintf(stderr, RED "Syntax Error:" RESET " The config file must begin with a number\n"
+            "Column: %zu\n", tokens_arr[0].column );
+        print_error_at(original_str, tokens_arr[0].column);
+        return -1;
+    }
+    if(tokens_arr[count-1].type != NUMBER){
+        fprintf(stderr, RED "Syntax Error:" RESET " The config file must end with a number\n"
+            "Column: %zu\n", tokens_arr[count-1].column );
+        print_error_at(original_str, tokens_arr[count-1].column);
+        return -1;
+    }
     for(size_t i = 0; i < count; i++){
         if (tokens_arr[i].type == SW){
             if(i + 1 >= count || tokens_arr[i + 1].type != OPEN){
@@ -135,6 +162,12 @@ int check_syntax(Token *tokens_arr, char *original_str, size_t count){
                     "Column: %zu\n", tokens_arr[i].column);
                 print_error_at(original_str, tokens_arr[i].column);
                 return -1;
+            }//the previous of the close parenthesis should be a number
+            if(tokens_arr[i-1].type != NUMBER){
+                fprintf(stderr, RED "Syntax Error: " RESET "Missing number before closing parenthesis\n"
+                    "Column: %zu\n", tokens_arr[i].column);
+                print_error_at(original_str, tokens_arr[i].column-1);
+                return -1;
             }
         }
     }
@@ -148,14 +181,38 @@ int check_syntax(Token *tokens_arr, char *original_str, size_t count){
 }
 
 int main(int argc, char *argv[]) {
+    if(argc < 2){
+        fprintf(stderr, "Usage: %s <file_to_tokenize>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
     size_t count;
     Token *tokens = NULL;
     printf("%s\n",argv[1]);
-    if(argc > 1){
+    FILE *file = fopen(argv[1], "r");
+    if (!file) {
+        fprintf(stderr, "Error opening file: %s\n", argv[1]);
+        fclose(file);
+        return EXIT_FAILURE;
+    }
+    char *line = NULL;
+    size_t len = 0;
+    while (getline(&line, &len, file) != -1) {
+        line[strcspn(line, "\n")] = 0;
+        printf("Processing line: %s", line);
+        tokens = tokenize(line, &count);
+        if(tokens){ 
+            print_tokens(tokens, count);
+            check_syntax(tokens, line, count);
+        }
+        if(tokens) free(tokens);
+    }
+    free(line);
+    fclose(file);
+    /*if(argc > 1){
         tokens = tokenize(argv[1], &count);
     }
     else{
-        fprintf(stderr, "Usage: %s <string_to_tokenize>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <file_to_tokenize>\n", argv[0]);
         return EXIT_FAILURE;
     }
     if(tokens){ 
@@ -164,5 +221,5 @@ int main(int argc, char *argv[]) {
     }
 
     if(tokens) free(tokens);
-    return 0;
+    return 0;*/
 }
