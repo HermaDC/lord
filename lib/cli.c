ca45a6cee49cb12409ceba8d588138d89a823509
HermaDC <diazcabezas.m1@gmail.com>
2026-07-19 16:15:42 +0200

    Feature: new flags save to json and update on load

--- lib/cli.c
+++ lib/cli.c
@@ -18,12 +18,14 @@ CLIOptions parse_args(int argc, char *argv[]) {
                                            {"command", required_argument, 0, 'c'},
                                            {"update", required_argument, 0, 'u'},
                                            {"verbose", no_argument, 0, 0},
+                                           {"update-on-load", no_argument, 0, 0},
+                                           {"to_json", required_argument, 0, 'J'},
                                            {0, 0, 0, 0}};
 
     int opt;
     int long_index = 0;
 
-    while((opt = getopt_long(argc, argv, "hf:sivc:u:", long_options, &long_index)) !=
+    while((opt = getopt_long(argc, argv, "hf:sivc:u:J:", long_options, &long_index)) !=
           -1) {
         switch(opt) {
         case 'h':
@@ -47,10 +49,19 @@ CLIOptions parse_args(int argc, char *argv[]) {
         case 'u':
             opts.update_time = atoi(optarg);
             break;
+        case 'J':
+            opts.to_json = optarg;
+            break;
+
         case 0: // long options without short version
-            if(long_options[long_index].name &&
-               strcmp(long_options[long_index].name, "verbose") == 0) {
+            if(!long_options[long_index].name) break;
+
+            if(strcmp(long_options[long_index].name, "verbose") == 0) {
                 opts.verbose = 1;
+            }
+            if(strcmp(long_options[long_index].name, "update-on-load") == 0) {
+                opts.update_on_load = 1;
+                break;
             }
             break;
         default:
@@ -65,13 +76,15 @@ CLIOptions parse_args(int argc, char *argv[]) {
 
 void print_help() {
     printf("Use:\n");
-    printf("  -h, --help              Show help\n");
-    printf("  -f, --file PATH         Load config file\n");
-    printf("  -i, --interactive       Runs in interactive mode\n");
-    printf("  -v, --version           Shows version\n");
-    printf("  -c, --command CMD       Runs CMD\n");
-    printf("  -u, --update MS         Time in miliseconds between updates\n");
-    printf("  -s, --save              Save system layout to file\n");
-    printf("      --verbose           Be more verbose\n");
+    printf("  -h, --help                         Show help\n");
+    printf("  -f, --file PATH            Load config file\n");
+    printf("  -i, --interactive          Runs in interactive mode\n");
+    printf("  -v, --version              Shows version\n");
+    printf("  -c, --command CMD          Runs CMD\n");
+    printf("  -u, --update MS            Time in miliseconds between updates\n");
+    printf("  -s, --save                 Save system layout to file\n");
+    printf("  -J, --to-json PATH         Transform the given layout into a file");
+    printf("      --verbose              Be more verbose\n");
+    printf("      --update-on-load       Update systems when loaded from file\n");
     printf("Report bugs at %s/issues\n", REPO_URL);
 }
--- lib/cli.h
+++ lib/cli.h
@@ -1,17 +1,21 @@
 #ifndef CLI_H
 #define CLI_H
 
+#include "stdbool.h"
 typedef struct {
-    int help;
-    int interactive;
-    int version;
-    int verbose;
-    int save;
     int update_time;
+
+    bool help;
+    bool interactive;
+    bool version;
+    bool verbose;
+    bool save;
+    bool update_on_load;
 
     char *file;
     char *command;
     char *script;
+    char *to_json;
 
 } CLIOptions;
 
